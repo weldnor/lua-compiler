3c3e2d24110550a5b971fff1a1e99a6b13e1b7f5
Udarczev Anatoliy <toly.udarczev@yandex.ru>
2021-03-02 18:54:55 +0300

    Fix project structure

--- src/main/java/stud/team/luacompiler/App.java
+++ src/main/java/stud/team/luacompiler/App.java
@@ -4,10 +4,9 @@ package stud.team.luacompiler;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
