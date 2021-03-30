c8ec596f0cdbcf784e33ff5e0fb83aad89ffc3f5
weldnor <kolesov.anton.s@gmail.com>
2021-03-30 15:18:20 +0300

    Add generator lib

--- src/main/java/stud/team/luacompiler/App.java
+++ src/main/java/stud/team/luacompiler/App.java
@@ -1,11 +1,14 @@
 package stud.team.luacompiler;
 
+import stud.team.luacompiler.*;
+
 /**
  * Hello world!
  */
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String javaClassContent = "public class SampleClass { void DoSomething(){} }";
+        Java8Lexer java8Lexer = new Java8Lexer(CharStreams.fromString(javaClassContent));
     }
 }
