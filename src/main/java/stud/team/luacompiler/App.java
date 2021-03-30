49bf85fb96c1ff6e0cecb2ce54b3c8d2c00c83d5
Udarczev Anatoliy <toly.udarczev@yandex.ru>
2021-03-30 21:58:06 +0300

    Add example tree print

--- src/main/java/stud/team/luacompiler/App.java
+++ src/main/java/stud/team/luacompiler/App.java
@@ -1,8 +1,29 @@
 package stud.team.luacompiler;
 
+import anltr4.LuaLexer;
+import anltr4.LuaParser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import stud.team.luacompiler.util.ThreeUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Starting application...");
+        String script = "" +
+                "b = 10;" +
+                "c = (10 + 3) * (2 + 4)" +
+                "while true do k = 1 end";
+        LuaLexer lexer = new LuaLexer(CharStreams.fromString(script));
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        LuaParser parser = new LuaParser(tokens);
+        ParseTree root = parser.block();
+
+        List<String> ruleNamesList = Arrays.asList(parser.getRuleNames());
+        String prettyTree = ThreeUtils.toPrettyTree(root, ruleNamesList);
+        System.out.println(prettyTree);
     }
 }
