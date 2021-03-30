d04d33ee9fef87ff204a8af1cedb16524d254df8
Udarczev Anatoliy <toly.udarczev@yandex.ru>
2021-03-30 15:31:55 +0300

    Add original lua grammatic

--- src/main/java/stud/team/luacompiler/App.java
+++ src/main/java/stud/team/luacompiler/App.java
@@ -1,14 +1,8 @@
 package stud.team.luacompiler;
 
-import stud.team.luacompiler.*;
-
-/**
- * Hello world!
- */
 public class App {
 
     public static void main(String[] args) {
-        String javaClassContent = "public class SampleClass { void DoSomething(){} }";
-        Java8Lexer java8Lexer = new Java8Lexer(CharStreams.fromString(javaClassContent));
+        System.out.println("Starting application...");
     }
 }
